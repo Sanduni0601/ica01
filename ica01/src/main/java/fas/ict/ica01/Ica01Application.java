dc3b153fe301993cb615f1a2c1c00115eba6a6d3
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-05-16 20:06:19 +0530

    Initialize spring boot app

---
+++ ica01/src/main/java/fas/ict/ica01/Ica01Application.java
@@ -0,0 +1,13 @@
+package fas.ict.ica01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ica01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ica01Application.class, args);
+	}
+
+}
---
+++ ica01/src/test/java/fas/ict/ica01/Ica01ApplicationTests.java
@@ -0,0 +1,13 @@
+package fas.ict.ica01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ica01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
